38cff85b72d36259e75ab885bd651617357e1016
buikimquangloc <118284013+buikimquangloc@users.noreply.github.com>
2023-04-30 12:36:46 +0700

    project

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const cors = require("cors"); // yêu cầu sử dụng cors
 
 const contactrouter = require("./app/router/route");
 
+const apierror = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -15,5 +17,16 @@ app.get("/", (req, res) => {  //định nghĩa route chho GET
 })
 
 app.use("/api/contact", contactrouter);
+app.use("/api/contact", contactrouter);
+
+app.use((req, res, nnext) => {
+    return next(new apierror(404, "Resource not found"));
+});
+
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal server error",
+    });
+});
 
 module.exports = app; // xuất mode ra ngoài và sử dụng file khác của node.js
